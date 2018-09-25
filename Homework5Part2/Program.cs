bf3cbe931457ff2c27160e605729e32510ff8294
Pope, David <dpope@dstsystems.com>
2018-09-25 13:47:54 -0500

    first commit

---
+++ Homework5Part2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a sentence without punctuation: ");
+            String sentence = Console.ReadLine();
+            string[] words = sentence.Split();
+
+            var distinctWords = from word in words
+                                orderby word ascending
+                                select word.ToLower(); ;
+
+            Console.WriteLine("\nDistinct words in sentence in alphabetical order: ");
+            Console.WriteLine("---------------------------------------------------------------");
+            foreach (string word in distinctWords.Distinct())
+            {
+                Console.WriteLine("- " + word);
+            }
+            Console.WriteLine("\nEnter any key to end program");
+            Console.ReadKey();
+        }
+    }
+}
